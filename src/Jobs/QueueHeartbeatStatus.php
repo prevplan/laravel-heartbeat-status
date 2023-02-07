48fb3a292624477434e3c898b8b478729f2ab594
Holger Schmermbeck <ruaq@prevplan.de>
2023-02-07 22:04:39 +0100

    add heartbeat job / queue job

---
+++ src/Jobs/QueueHeartbeatStatus.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Prevplan\HeartbeatStatus\Jobs;
+
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Cache;
+
+class QueueHeartbeatStatus implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle(): void
+    {
+        Cache::forever('queue_heartbeat', Carbon::now());
+
+        // Schedule next run
+        static::dispatch()->delay(Carbon::now()->addMinutes(config('heartbeat-status.heart-rate')));
+    }
+}
