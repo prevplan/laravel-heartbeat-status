911bab17f13171483dd0a5ff71aca7378782ca33
Holger Schmermbeck <ruaq@prevplan.de>
2023-02-07 22:10:43 +0100

    add service provider

--- src/HeartbeatStatusServiceProvider.php
+++ src/HeartbeatStatusServiceProvider.php
@@ -1,25 +1,39 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Prevplan\HeartbeatStatus;
 
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\Route;
+use Prevplan\HeartbeatStatus\Commands\HeartbeatStatusCommand;
+use Prevplan\HeartbeatStatus\Http\Controllers\HeartbeatStatusController;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Prevplan\HeartbeatStatus\Commands\HeartbeatStatusCommand;
 
 class HeartbeatStatusServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-heartbeat-status')
             ->hasConfigFile()
             ->hasViews()
-            ->hasMigration('create_laravel-heartbeat-status_table')
             ->hasCommand(HeartbeatStatusCommand::class);
+    }
+
+    public function packageRegistered()
+    {
+        Route::macro('heartbeat_status', function (string $baseUrl = 'heartbeat') {
+            Route::prefix($baseUrl)->group(function () {
+                Route::get('/', [HeartbeatStatusController::class, 'html']);
+            });
+        });
+
+        Route::heartbeat_status();
+
+        $this->app->afterResolving(Schedule::class, function (Schedule $schedule) {
+            $schedule->command(Commands\HeartbeatStatusCommand::class)->everyMinute();
+        });
     }
 }
